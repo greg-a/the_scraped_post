afb159dc19eb9071d2326d2e281432c6d18dc3bf
Greg Allebach <greg.allebach@gmail.com>
2020-08-13 19:24:40 -0400

    changed port variable

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var mongoose = require("mongoose");
 var axios = require("axios");
 var cheerio = require("cheerio");
 var db = require("./models");
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(express.urlencoded({ extended: false }));
